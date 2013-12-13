8ff7b0e1ef16b27610f07ff718aa16baab693bcf
Zvonimir Buric <zburic@comwrap.com>
2013-12-13 14:00:10 +0100

    remove spaces and add tabs

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,9 @@
 <?php
 
 	require_once '../app/Mage.php';
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    Mage::app();
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	Mage::app();
 
 	Mage::helper('cli')->putSuccess("Test success message");
 	Mage::helper('cli')->putError("Test error message");
