dc3f5e75ae1b3e9b446cf8f6fc0d42778ec0a181
Zvonimir Buric <zburic@comwrap.com>
2013-12-13 13:47:46 +0100

    Added usage example.

---
+++ tests/test.php
@@ -0,0 +1,13 @@
+<?php
+
+	require_once '../app/Mage.php';
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    Mage::app();
+
+	Mage::helper('cli')->putSuccess("Test success message");
+	Mage::helper('cli')->putError("Test error message");
+	Mage::helper('cli')->putWarning("Test warning message");
+	Mage::helper('cli')->putNotice("Test notification message");
+	
+	Mage::helper('cli')->puts("Custom styled message", array('underline', 'bg_light_magenta', 'bold'));
